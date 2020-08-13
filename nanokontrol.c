107daa152eedf7f79f80f4a6a4a3e89de891537e
Wolfgang Schnerring <wosc@wosc.de>
2020-08-13 09:21:59 +0200

    Simplify spelling by extracting some helper functions

--- nanokontrol.c
+++ nanokontrol.c
@@ -14,9 +14,12 @@
 
 @init  // ------------------------------------------------------------
 
+DEBUG = 0;
+
 MIDI_CC = 176;
 MIDI_ON = 127;
 
+// These CC numbers are the Korg KONTROL Editor factory defaults.
 MIDI_VOLUME = 0;
 MIDI_PAN = 16;
 MIDI_SOLO = 32;
@@ -36,149 +39,110 @@ MIDI_MARKER_NEXT = 62;
 MIDI_TRACK_PREV = 58;
 MIDI_TRACK_NEXT = 59;
 
+// Find IDs with "Copy selected action command ID" after right-clicking an
+// action in the "Actions" menu.
+REAPER_GOTO_PROJECT_START = 40042;
+REAPER_GOTO_PROJECT_END = 40043;
+REAPER_GOTO_PREV_MARKER = 40172;
+REAPER_GOTO_NEXT_MARKER = 40173;
+REAPER_INSERT_MARKER = 40157;
 
-@oscmsg  // ------------------------------------------------------------
-//printf("osc %s\n", oscstr);  // DEBUG
 
+function midisend_track(control, track) (
+  msg1 = MIDI_CC;
+  msg2 = control + track - 1;
+  value = oscparm(0);
+  msg3 = value * MIDI_ON;
+  midisend(midi_out);
+);
+
+
+function midisend_toggle(control) (
+  msg1 = MIDI_CC;
+  msg2 = control;
+  state = oscparm(0);
+  msg3 = state * MIDI_ON;
+  midisend(midi_out);
+);
+
+
+function oscsend_track(control, verb, value) (
+  track = msg2 - control + 1;
+  sprintf(#url, "n/track/%%d/%s", verb);
+  oscsend(osc_out, #url, value, track);
+);
+
+
+function oscsend_action(action) (
+  oscsend(osc_out, "i/action", action);
+);
+
+
+function midibool2osc() (
+  msg3 == MIDI_ON ? 1 : 0;
+);
+
+
+/**
+ * Receives MIDI messages from the hardware and then triggers Reaper commands.
+ */
+@midimsg
+
+DEBUG ? printf("midi %d %d %d\n", msg1, msg2, msg3);
 
 // Tracks
-
-oscmatch("/track/%d/solo", track) ? (
-  state = oscparm(0);
-  msg1 = MIDI_CC;
-  msg2 = MIDI_SOLO + track - 1;
-  msg3 = state * MIDI_ON;
-  midisend(midi_out);
-);
-
-oscmatch("/track/%d/mute", track) ? (
-  state = oscparm(0);
-  msg1 = MIDI_CC;
-  msg2 = MIDI_MUTE + track - 1;
-  msg3 = state * MIDI_ON;
-  midisend(midi_out);
-);
-
-oscmatch("/track/%d/recarm", track) ? (
-  state = oscparm(0);
-  msg1 = MIDI_CC;
-  msg2 = MIDI_RECARM + track - 1;
-  msg3 = state * MIDI_ON;
-  midisend(midi_out);
-);
-
-
-// Transport
-
-oscmatch("/play") ? (
-  state = oscparm(0);
-  msg1 = MIDI_CC;
-  msg2 = MIDI_PLAY;
-  msg3 = state * MIDI_ON;
-  midisend(midi_out);
-);
-
-oscmatch("/record") ? (
-  state = oscparm(0);
-  msg1 = MIDI_CC;
-  msg2 = MIDI_RECORD;
-  msg3 = state * MIDI_ON;
-  midisend(midi_out);
-);
-
-oscmatch("/repeat") ? (
-  state = oscparm(0);
-  msg1 = MIDI_CC;
-  msg2 = MIDI_CYCLE;
-  msg3 = state * MIDI_ON;
-  midisend(midi_out);
-);
-
-
-@midimsg  // ------------------------------------------------------------
-//printf("midi %d %d %d\n", msg1, msg2, msg3);  // DEBUG
-
-
-// Tracks
-
 msg2 >= MIDI_VOLUME && msg2 <= MIDI_VOLUME + 8 ? (
-  track = msg2 - MIDI_VOLUME + 1;
-  value = msg3 / MIDI_ON;
-  oscsend(osc_out, "n/track/%d/volume", value, track);
+  oscsend_track(MIDI_VOLUME, "volume", msg3 / MIDI_ON);
 );
-
 // XXX The second pan knob seems to randomly/all the time send midi messages,
 // regardless of which CC is assigned to it. Maybe my hardware is broken?
 msg2 >= MIDI_PAN && msg2 <= MIDI_PAN + 8 && msg2 != MIDI_PAN + 1 ? (
-  track = msg2 - MIDI_PAN + 1;
-  value = msg3 / MIDI_ON;
-  oscsend(osc_out, "n/track/%d/pan", value, track);
+  oscsend_track(MIDI_PAN, "pan", msg3 / MIDI_ON);
 );
-
 msg2 >= MIDI_SOLO && msg2 <= MIDI_SOLO + 8 ? (
-  track = msg2 - MIDI_SOLO + 1;
-  state = msg3 == MIDI_ON ? 1 : 0;
-  oscsend(osc_out, "b/track/%d/solo", state, track);
+  oscsend_track(MIDI_SOLO, "solo", midibool2osc());
 );
-
 msg2 >= MIDI_MUTE && msg2 <= MIDI_MUTE + 8 ? (
-  track = msg2 - MIDI_MUTE + 1;
-  state = msg3 == MIDI_ON ? 1 : 0;
-  oscsend(osc_out, "b/track/%d/mute", state, track);
+  oscsend_track(MIDI_MUTE, "mute", midibool2osc());
 );
-
 msg2 >= MIDI_RECARM && msg2 <= MIDI_RECARM + 8 ? (
-  track = msg2 - MIDI_RECARM + 1;
-  state = msg3 == MIDI_ON ? 1 : 0;
-  oscsend(osc_out, "b/track/%d/recarm", state, track);
+  oscsend_track(MIDI_RECARM, "recarm", midibool2osc());
 );
 
 
 // Transport
-
-msg2 == MIDI_PLAY && msg3 == MIDI_ON ? (
-  oscsend(osc_out, "t/play");
+msg3 == MIDI_ON ? (
+  msg2 == MIDI_PLAY ? oscsend(osc_out, "t/play");
+  msg2 == MIDI_STOP ? oscsend(osc_out, "t/stop");
+  msg2 == MIDI_RECORD ? oscsend(osc_out, "t/record");
+  msg2 == MIDI_TRACK_PREV ? oscsend_action(REAPER_GOTO_PROJECT_START);
+  msg2 == MIDI_TRACK_NEXT ? oscsend_action(REAPER_GOTO_PROJECT_END);
+  msg2 == MIDI_MARKER_PREV ? oscsend_action(REAPER_GOTO_PREV_MARKER);
+  msg2 == MIDI_MARKER_NEXT ? oscsend_action(REAPER_GOTO_NEXT_MARKER);
+  msg2 == MIDI_MARKER_SET ? oscsend_action(REAPER_INSERT_MARKER);
 );
 
-msg2 == MIDI_STOP && msg3 == MIDI_ON ? (
- oscsend(osc_out, "t/stop");
-);
+// cycle button is configured as "toggle"
+msg2 == MIDI_CYCLE ? oscsend(osc_out, "b/repeat", midibool2osc());
 
-msg2 == MIDI_RECORD && msg3 == MIDI_ON ? (
-  oscsend(osc_out, "t/record");
-);
+// rew/fwd are configured as "momentary", so we can hold them down for moving
+msg2 == MIDI_REWIND ? oscsend(osc_out, "b/rewind", midibool2osc());
+msg2 == MIDI_FORWARD ? oscsend(osc_out, "b/forward", midibool2osc());
 
-msg2 == MIDI_REWIND ? (
-  state = msg3 == MIDI_ON ? 1 : 0;
-  oscsend(osc_out, "b/rewind", state);
-);
 
-msg2 == MIDI_FORWARD ? (
-  state = msg3 == MIDI_ON ? 1 : 0;
-  oscsend(osc_out, "b/forward", state);
-);
+/**
+ * Receives OSC messages from Reaper and then turns hardware LEDs on/off.
+ */
+@oscmsg
 
-msg2 == MIDI_CYCLE ? (
-  state = msg3 == MIDI_ON ? 1 : 0;
-  oscsend(osc_out, "b/repeat", state);
-);
+DEBUG ? printf("osc %s\n", oscstr);
 
-msg2 == MIDI_TRACK_PREV && msg3 == MIDI_ON ? (
-  oscsend(osc_out, "i/action", 40042);  // goto project start
-);
+// Tracks
+oscmatch("/track/%d/solo", track) ? midisend_track(MIDI_SOLO, track);
+oscmatch("/track/%d/mute", track) ? midisend_track(MIDI_MUTE, track);
+oscmatch("/track/%d/recarm", track) ? midisend_track(MIDI_RECARM, track);
 
-msg2 == MIDI_TRACK_NEXT && msg3 == MIDI_ON ? (
-  oscsend(osc_out, "i/action", 40043);  // goto project end
-);
-
-msg2 == MIDI_MARKER_PREV && msg3 == MIDI_ON ? (
- oscsend(osc_out, "i/action", 40172);  // goto prev marker
-);
-
-msg2 == MIDI_MARKER_NEXT && msg3 == MIDI_ON ? (
-  oscsend(osc_out, "i/action", 40173);  // goto next marker
-);
-
-msg2 == MIDI_MARKER_SET && msg3 == MIDI_ON ? (
-   oscsend(osc_out, "i/action", 40157);  // insert marker at current position
-);
+// Transport
+oscmatch("/play") ? midisend_toggle(MIDI_PLAY);
+oscmatch("/record") ? midisend_toggle(MIDI_RECORD);
+oscmatch("/repeat") ? midisend_toggle(MIDI_CYCLE);
